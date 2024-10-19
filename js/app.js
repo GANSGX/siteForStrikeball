2ff9ea84633405e37907c896b16ecf38a5336aae
GANSGX <takeshigans@gmail.com>
2024-10-19 11:30:22 -0700

    ver.0.4

--- js/app.js
+++ js/app.js
@@ -18,3 +18,37 @@ var swiper = new Swiper(".mySwiper", {
     },
 });
 
+
+document.addEventListener("DOMContentLoaded", function () {
+    const modal = document.getElementById("modal");
+    const openButton = document.querySelector(".unique-order-button");
+    const closeButton = document.querySelector(".close-button");
+
+    // Функция для открытия модального окна
+    openButton.addEventListener("click", function (event) {
+        event.preventDefault(); // Отменяем действие по умолчанию
+        modal.style.display = "block"; // Показываем модальное окно
+    });
+
+    // Функция для закрытия модального окна
+    function closeModal() {
+        modal.style.display = "none"; // Скрываем модальное окно
+    }
+
+    // Закрытие модального окна при клике на кнопку закрытия
+    closeButton.addEventListener("click", closeModal);
+
+    // Закрытие модального окна при клике вне его содержимого
+    window.addEventListener("click", function (event) {
+        if (event.target === modal) {
+            closeModal();
+        }
+    });
+
+    // Закрытие модального окна при нажатии клавиши Esc
+    window.addEventListener("keydown", function (event) {
+        if (event.key === "Escape") {
+            closeModal();
+        }
+    });
+});
